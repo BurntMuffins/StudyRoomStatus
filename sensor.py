3667d1e24181ad8e6158e96640ec5308aa0f66d0
BurntMuffins <aidanjs1208@gmail.com>
2023-04-06 22:33:06 -0500

    possible fix

--- sensor.py
+++ sensor.py
@@ -5,18 +5,29 @@
 # the room is occupied.
 ####################################################################
 
-import RPi.GPIO as GPIO
-import time
+# import RPi.GPIO as GPIO
+# import time
 
-def status():
-    stat = False
-    while True:
-        start_time = time.time()
-        while time.time() < start_time + 600:
-            if status:
-                return True # This is reached when the sensor detects motion
-            time.sleep(1)
-        return False # This is reached when the room is empty for 10 minutes
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(2, GPIO.IN)
+
+# def status():
+#     stat = False
+#     while True:
+#         start_time = time.time()
+#         while time.time() < start_time + 600:
+#             if status:
+#                 return True # This is reached when the sensor detects motion
+#             time.sleep(1)
+#         return False # This is reached when the room is empty for 10 minutes
+
+from gpiozero import MotionSensor
+
+pir = MotionSensor(2)
+while True:
+    pir.wait_for_motion()
+    print("You moved")
+    pir.wait_for_no_motion()
     
 
 
