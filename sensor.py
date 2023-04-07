7c68b40cd2161f7d7c2a6bd033179f73fb28dd86
BurntMuffins <aidanjs1208@gmail.com>
2023-04-07 10:02:34 -0500

    reworked the sensor logic

--- sensor.py
+++ sensor.py
@@ -3,13 +3,44 @@
 # room the sensor is placed in is occupied or not. 
 ####################################################################
 
-from gpiozero import MotionSensor
+import RPi.GPIO as GPIO
+import time
 
-pir = MotionSensor(4)
+# set up the GPIO pins
+GPIO.setmode(GPIO.BCM)
+SENSOR = 4
+GPIO.setup(SENSOR, GPIO.IN)
+
+# set the room status
+occupied = False
+
+# A function that will check if the sensor was triggered
+def motion_detected():
+    return GPIO.input(SENSOR) == GPIO.HIGH
+
+# a function that updates the room status
+def update_room_status():
+    global occupied
+    if motion_detected():
+        occupied = True
+        print("Room occupied")
+    else:
+        occupied = False
+        print("Room available")
+
+# Loop forever, checking the sensor every second
 while True:
-    if pir._active_state:
-        print("working")
-    
+    update_room_status()
+    time.sleep(1)
 
-
-    
+    # If the room was occupied and motion was not detected for 5 minutes, mark it was available
+    if not motion_detected() and occupied:
+        time_since_motion = 0
+        while time_since_motion < 300:
+            time.sleep(1)
+            time_since_motion += 1
+            if motion_detected():
+                break
+            else:
+                occupied = False
+                print("Room available")    
